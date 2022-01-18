d3abde67958d6a179b905598116916db73ad5937
Josh-Human <jhumanb1@gmail.com>
2022-01-18 12:40:24 +0000

    for nady

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 console.log('A test for team 11')
 
-const array=[]
+const array = []
 array.forEach((element) => {
   array.push('some text')
 })
 
-
-for (let i = 0; i<array.length; i++) {
-    console.log("test1");
+for (let i = 0; i < array.length; i++) {
+  console.log('test1')
 }
 for (let i = 0; i < array.length; i++) {
-    console.log("test2");
+  console.log('test2')
 }
