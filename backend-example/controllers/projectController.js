766796534918ef11da5de62d47dcf03a2f9e0d94
Josh-Human <jhumanb1@gmail.com>
2022-01-20 15:25:08 +0000

    files formatted

--- backend-example/controllers/projectController.js
+++ backend-example/controllers/projectController.js
@@ -1,17 +1,17 @@
-const { Project } = require('../models/project')
+const { Project } = require('../models/project');
 
 // if run into issue get rid of sort
 const projectGetAll = (req, res) => {
-  Project.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.status(200).send(result)
-    })
-    .catch((err) => {
-      res.status(400).send(
+    Project.find()
+        .sort({ createdAt: -1 })
+        .then((result) => {
+            res.status(200).send(result);
+        })
+        .catch((err) => {
+            res.status(400).send(
                 `There was an error with loading Projects. ${err}`
-      )
-    })
-}
+            );
+        });
+};
 
-module.exports = { projectGetAll }
+module.exports = { projectGetAll };
--- backend-example/mongodb.js
+++ backend-example/mongodb.js
@@ -1,26 +1,28 @@
-
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb');
 // const fs = require('fs')
 
-const credentials = '/home/josh/Downloads/X509-cert-7396365317458111254.pem'
+const credentials = '/home/josh/Downloads/X509-cert-7396365317458111254.pem';
 
-const client = new MongoClient('mongodb+srv://cluster0.e4hcm.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
-  sslKey: credentials,
-  sslCert: credentials
-})
+const client = new MongoClient(
+    'mongodb+srv://cluster0.e4hcm.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',
+    {
+        sslKey: credentials,
+        sslCert: credentials,
+    }
+);
 
-async function run () {
-  try {
-    await client.connect()
-    const database = client.db('sample_analytics')
-    const collection = database.collection('accounts')
-    const docCount = await collection.countDocuments({})
-    console.log(docCount)
-    // perform actions using client
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close()
-  }
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('sample_analytics');
+        const collection = database.collection('accounts');
+        const docCount = await collection.countDocuments({});
+        console.log(docCount);
+        // perform actions using client
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
