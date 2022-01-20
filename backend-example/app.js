66ce3ed2aec5a07c56f98e7a51c04ffdc5e81ad6
Josh-Human <jhumanb1@gmail.com>
2022-01-20 15:14:03 +0000

    refactor to use express JSON parser

--- backend-example/app.js
+++ backend-example/app.js
@@ -5,7 +5,6 @@ const express = require('express')
 const cors = require('cors')
 
 const projectRoutes = require('./routes/projectRoutes')
-const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = 8080
@@ -16,23 +15,23 @@ const credentials = dbConfig.credentials
 const dbURI = dbConfig.url
 
 const options = {
-  sslKey: credentials,
-  sslCert: credentials,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    sslKey: credentials,
+    sslCert: credentials,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }
 
 mongoose
-  .connect(dbURI, options)
-  .then(function (result) {
-    console.log('Database is connected')
-  })
-  .catch((err) => console.log(err))
+    .connect(dbURI, options)
+    .then(function (result) {
+        console.log('Database is connected')
+    })
+    .catch((err) => console.log(err))
 
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => res.send('Hello from homepage.'))
 app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
 
-app.use(bodyParser.json())
 app.use('/projects', projectRoutes)
